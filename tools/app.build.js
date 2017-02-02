e4fed585b0564dca11d045b6293fbae6f94e9077
Ar-to <aricode0@gmail.com>
2017-02-01 22:46:34 -0800

    Corrected navigation issues, updated templates

---
+++ tools/app.build.js
@@ -0,0 +1,11 @@
+({
+    baseUrl: "../js",
+    out: 'main-build2.js',
+    mainConfigFile: '../js/build.js',
+    include: ['build'],
+    name: 'almond',
+    paths: {
+      nav: 'lib/modules/navmod',
+      smoothscll: 'lib/modules/smoothscroll'
+    },
+})
