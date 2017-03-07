4f84b873a98540796c18287607118d705eba1725
Ar-to <aricode0@gmail.com>
2017-03-06 18:45:58 -0800

    updated r.js and manual requirejs commands via npm script

--- tools/rjs.build.js
+++ tools/rjs.build.js
@@ -1,11 +1,12 @@
 ({
-    baseUrl: "../js",
-    out: 'main-build2.js',
-    mainConfigFile: '../js/build.js',
-    include: ['build'],
-    name: 'almond',
-    paths: {
-      nav: 'lib/modules/navmod',
-      smoothscll: 'lib/modules/smoothscroll'
-    },
+  baseUrl: "../src/js",//relative to gulpfile
+  mainConfigFile: '../src/js/require.config.js',//relative to gulpfile
+  paths: {
+    nav: 'lib/modules/navmod',//relative to baseUrl
+    smoothscll: 'lib/modules/smoothscroll',
+    nav_color: 'lib/modules/nav_color'
+  },
+  include: ['require.config'],//relative to baseUrl
+  name: "../../tools/almond",//relative to baseURL
+  out: "../dist/js/rjs_bundle.js"//relative to rjs.build.js
 })
